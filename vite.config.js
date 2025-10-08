db8c3d6d93b6d610742e18160ed1350b7f02b17f
randa <randa@RANOODA>
2025-10-08 19:31:48 +0300

    Byway project

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Byway_User/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
   server: {
     port: 3000,
-    open: true
-  }
+  },
+base:"Byway_User"
+
 })
