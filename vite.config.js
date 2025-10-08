dd971807ea25f6cdf42574b804661dce7e5f8509
randa <randa@RANOODA>
2025-10-08 19:15:23 +0300

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Byway_User/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   server: {
     port: 3000,
-  },
+    open: true
+  }
 })
